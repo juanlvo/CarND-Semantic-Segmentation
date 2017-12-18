310978f499f5f2642f9d543f13bbddc5b1139e7d
Juan Vivas <juanlvo@gmail.com>
2017-12-18 16:51:58 +0100

    feat: part of the implemenatation of some methods in main
    part of the implementation of:
      - layers
      - optimizer
      - train_nn
      - run

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import helper
 import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
+from reportlab.lib.pdfencrypt import padding
 
 
 # Check TensorFlow Version
@@ -54,6 +55,10 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', kernel_regulizer=tf.contrib.layers.l2_regulizer(1e-3))
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same', kernel_regulizer=tf.contrib.layers.l2_regulizer(1e-3))
+    tf.Print(output, [tf.shape(output) [1:3]])
+    
     return None
 tests.test_layers(layers)
 
@@ -68,6 +73,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
+    logits = tf.reshape(input, (-1, num_classes))
     return None, None, None
 tests.test_optimize(optimize)
 
@@ -88,6 +94,10 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
+    for epochs in epochs:
+        for image, label in get_batches_fn(batch_size):
+            #Training
+            pass
     pass
 tests.test_train_nn(train_nn)
 
@@ -116,7 +126,10 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-
+        
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        
         # TODO: Train NN using the train_nn function
 
         # TODO: Save inference data using helper.save_inference_samples
